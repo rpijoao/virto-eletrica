bbd77dc2c6589575c135cd8ee70deda3d8e9d4ab
rpijoao <contato.joaofurtado@gmail.com>
2026-04-02 14:00:56 -0300

    rename: orcamento -> atendimento + redirect 301

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        orcamento: resolve(__dirname, 'orcamento.html'),
+        atendimento: resolve(__dirname, 'atendimento.html'),
       },
     },
   },
